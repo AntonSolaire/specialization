b4218082f40687bcbdb854458b2b44d176d87455
Anton Gadyatskiy <aoagadbart90@gmail.com>
2023-01-13 21:03:38 +0300

    Написал метод решающий задачу

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿string[] array1 = new string[5] {"111", "222", "helloworld", "alohha", "bla"};
-string[] array2 = new string[array1.Length];
+﻿string[] firstarray = new string[5] {"111", "222", "helloworld", "alohha", "bla"};
+string[] secondarray = new string[firstarray.Length];
+
+void ResultArray(string[] firstarray, string[] secondarray)
+{
+    int count = 0;
+    for (int i = 0; i < firstarray.Length; i++)
+    {
+    if(firstarray[i].Length <= 3)
+        {
+        secondarray[count] = firstarray[i];
+        count++;
+        }
+    }
+}
 
